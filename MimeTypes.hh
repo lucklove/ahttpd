f80155e09fe972ce363fb65dac27320ef4316b63
Jos <gnu.crazier@gmail.com>
2015-03-19 17:42:59 +0800

    delete

--- MimeTypes.hh
+++
@@ -1,8 +0,0 @@
-#ifndef HTTP_MIME_TYPES_HPP
-#define HTTP_MIME_TYPES_HPP
-
-#include <string>
-
-std::string extension_to_type(const std::string& extension);
-
-#endif // HTTP_MIME_TYPES_HPP
--- buffer.hh
+++
@@ -1,5 +0,0 @@
-#pragma once
-
-#include <boost/asio.hpp>
-
-using buffer_t = boost::asio::streambuf;
