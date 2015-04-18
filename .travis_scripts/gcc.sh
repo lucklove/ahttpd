3cc682e1ff1ac3c46eb5504e6b1bbb8a07b905a5
Jos <gnu.crazier@gmail.com>
2015-04-18 10:44:26 +0800

    update travis boost

--- .travis_scripts/gcc.sh
+++ .travis_scripts/gcc.sh
@@ -1,14 +1,8 @@
 #!/bin/bash
 
-wget http://mirrors.kernel.org/ubuntu/pool/main/g/gcc-4.9-arm64-cross/cpp-4.9-aarch64-linux-gnu_4.9.2-10ubuntu13cross0.7_amd64.deb
-wget http://mirrors.kernel.org/ubuntu/pool/main/g/gcc-4.9-arm64-cross/gcc-4.9-aarch64-linux-gnu_4.9.2-10ubuntu13cross0.7_amd64.deb
-wget http://mirrors.kernel.org/ubuntu/pool/main/g/gcc-defaults-arm64-cross/gcc-aarch64-linux-gnu_4.9.2-2_amd64.deb
-#wget ftp://gsapubftp-anonymous@ftp.broadinstitute.org/travis/gcc_4.9.1-1_amd64.deb
-
-sudo dpkg --install cpp-4.9-aarch64-linux-gnu_4.9.2-10ubuntu13cross0.7_amd64.deb
-sudo dpkg --install gcc-4.9-aarch64-linux-gnu_4.9.2-10ubuntu13cross0.7_amd64.deb
+wget ftp://gsapubftp-anonymous@ftp.broadinstitute.org/travis/gcc_4.9.1-1_amd64.deb
 sudo apt-get remove cpp libffi-dev
-sudo dpkg --install gcc-aarch64-linux-gnu_4.9.2-2_amd64.deb
+sudo dpkg --install gcc_4.9.1-1_amd64.deb
 
 echo "BEGIN Eliminating old libstdc++"
 sudo rm /usr/lib/gcc/i586-mingw32msvc/4.2.1-sjlj/libstdc++.a
