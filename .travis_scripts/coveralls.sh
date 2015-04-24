2fb50e0b6ed0ddb1d747eeb4fb7f6fd054dc6a1c
Jos <gnu.crazier@gmail.com>
2015-04-24 11:04:56 +0800

    添加测试用例

--- .travis_scripts/coveralls.sh
+++ .travis_scripts/coveralls.sh
@@ -6,5 +6,5 @@ if [ "$CXX" == "g++" ];
 then
 	sudo pip install cpp-coveralls
 	cd build
-	coveralls -r ../src --gcov-options '\-lp'
+	coveralls -r ../ -e CMakeFiles -e test -e example --gcov-options '\-lp'
 fi
