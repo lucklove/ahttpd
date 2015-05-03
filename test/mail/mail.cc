0abe5a67bd624008390104fdc01201ce483d49c3
Jos <gnu.crazier@gmail.com>
2015-05-03 18:31:21 +0800

    use ssl to test mail

--- test/mail/mail.cc
+++ test/mail/mail.cc
@@ -4,7 +4,7 @@
 
 BOOST_AUTO_TEST_CASE(mail_test)
 {
-	Mail m("ahttpd@163.com", "qjhjhnevjghjdoml", "smtp.163.com");
+	Mail m("ahttpd@163.com", "qjhjhnevjghjdoml", "smtp.163.com", "465", true);
 	m.send("1041324091@qq.com", "TEST REPORT", 
 		"A new test passed at " + gmtTime(time(nullptr)) + " GMT", 
 		[=](bool good) {
