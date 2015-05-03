e690b416fd735c1987abecd6b8550b8e78b5ea3b
Joshua <gnu.crazier@gmail.com>
2015-05-03 21:56:02 +0800

    添加Mime

--- src/MimeType.cc
+++ src/MimeType.cc
@@ -5,21 +5,27 @@ struct map_t {
 	const char* extension;
 	const char* mime_type;
 } maps[] = {
+	{ "323", "text/h323" },
+	{ "aif", "audio/x-aiff" },
+	{ "aifc", "audio/x-aiff" },
+	{ "aiff", "audio/x-aiff" },
+	{ "au", "audio/basic" },
+	{ "avi", "video/x-msvideo" },
 	{ "css", "text/css" },
+	{ "flv", "video/x-flv" },
 	{ "gif", "image/gif" },
 	{ "htm", "text/html" },
 	{ "html", "text/html" },
 	{ "jpg", "image/jpeg" },
-	{ "png", "image/png" },
 	{ "js", "text/javascript" },
+	{ "mp3", "audio/mp3" },
+	{ "mp4", "video/mp4" },
 	{ "oga", "audio/ogg" },
 	{ "ogg", "audio/ogg" },
 	{ "ogv", "video/ogg" },
-	{ "swf", "application/x-shockwave-flash" },
-	{ "mp4", "video/mp4" },
-	{ "flv", "video/x-flv" },
-	{ "mp3", "audio/mp3" },
+	{ "png", "image/png" },
 	{ "svg", "image/svg+xml" },
+	{ "swf", "application/x-shockwave-flash" },
 	{ "webm", "video/webm" },
 	{ "xml", "application/xml" }
 };
