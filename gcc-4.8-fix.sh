6b8dddded76b5870ee0afd78d494ad798ba3287a
Jos <gnu.crazier@gmail.com>
2015-11-18 17:09:29 +0800

    delete gcc-4.8-fix.sh

--- gcc-4.8-fix.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-sed -i "s/boost_system/boost_system boost_regex/g" src/CMakeLists.txt
-sed -i "s/<regex>/<boost\/regex.hpp>/g" src/*
-sed -i "s/std::regex/boost::regex/g" src/*
-sed -i "s/std::smatch/boost::smatch/g" src/*
